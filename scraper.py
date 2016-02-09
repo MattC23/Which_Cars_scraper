6cc1335ccaa4cc435f5ca74e1645757f8abca9f3
MattC23 <matt.clear@which.co.uk>
2016-02-09 15:53:02 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -24,43 +24,44 @@
 # called "data".
 
 ##################################### Method 1
-import mechanize
-import cookielib
-from BeautifulSoup import BeautifulSoup
-import html2text
+
+# import mechanize
+# import cookielib
+# from BeautifulSoup import BeautifulSoup
+# import html2text
 
 # Browser
-br = mechanize.Browser()
+# br = mechanize.Browser()
 
 # Cookie Jar
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
+# cj = cookielib.LWPCookieJar()
+# br.set_cookiejar(cj)
 
 # Browser options
-br.set_handle_equiv(True)
-br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+# br.set_handle_equiv(True)
+# br.set_handle_gzip(True)
+# br.set_handle_redirect(True)
+# br.set_handle_referer(True)
+# br.set_handle_robots(False)
+# br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-br.addheaders = [('User-agent', 'Chrome')]
+# br.addheaders = [('User-agent', 'Chrome')]
 
 # The site we will navigate into, handling it's session
-br.open('https://www.which.co.uk/login/#?intcmp=GNH.Login')
+# br.open('https://www.which.co.uk/login/#?intcmp=GNH.Login')
 
 # View available forms
-for f in br.forms():
-    print f
+# for f in br.forms():
+#     print f
 
 # Select the second (index one) form (the first form is a search query box)
-br.select_form(nr=1)
+# br.select_form(nr=1)
 
 # User credentials
-br.form['username'] = 'mylogin'
-br.form['password'] = 'mypass'
+# br.form['username'] = 'mylogin'
+# br.form['password'] = 'mypass'
 
 # Login
-br.submit()
+# br.submit()
 
-print(br.open('http://www.which.co.uk/cars/alfa-romeo-159-2006-2011-review').read())
+# print(br.open('http://www.which.co.uk/cars/alfa-romeo-159-2006-2011-review').read())
